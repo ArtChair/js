e332b8fec048c2a7308ac6cd84a0148169de7b52
Artem <104214210+ArtChair@users.noreply.github.com>
2022-05-15 22:05:21 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var numOne = prompt ('Введите первое число:')
+var numTwo = prompt ('Введите второе число:')
+if ( numOne > numTwo) {
+    console.log(numOne + ' больше ' + numTwo);
+} else if (numOne < numTwo) {
+    console.log(numOne + ' меньше ' + numTwo);
+} else  {
+    console.log(numOne + ' равно ' + numTwo)
+}
